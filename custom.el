09823b34f40f111abfe864e37999b0278959e337
limin <limin@limindeMacBook-Air.local>
2022-01-19 01:07:25 +0800

    add elpa

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(quickrun yaml-mode restclient protobuf-mode markdown-mode json-mode emmet-mode web-mode rust-mode go-mode eglot format-all drag-stuff hungry-delete crux yasnippet-snippets yasnippet which-key highlight-parentheses rainbow-delimiters projectile exec-path-from-shell company all-the-icons delight diminish gnu-elpa-keyring-update use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ elpa/markdown-mode-20220117.1451/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from markdown-mode.el  -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20220117.1451" "Major mode for Markdown-formatted text" '((emacs "25.1")) :commit "c002dc075397143147b5660df289d834d06b5909" :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :keywords '("markdown" "github flavored markdown" "itex") :url "https://jblevins.org/projects/markdown-mode/")
