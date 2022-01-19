f4401062a85c8daf2f11373f546d70fbac82ed2e
mathslimin <mathslimin@gmail.com>
2022-01-19 12:12:04 +0800

    Update early-init.el

--- early-init.el
+++ early-init.el
@@ -1,17 +1,24 @@
 ;;; early-init.el --- Emacs 27 introduces early-init.el, which runs before init.el
-;;; Commentary:
 ;; Runs before package and UI initializetion happens.
 ;;; Code:
 
-;; Defer garbage collection further back in the startup process
+;; For speed up the startup, please do NOT forget reset it to default
+;; after Emacs after-init-hook, or it may cause freezes.
 (setq gc-cons-threshold most-positive-fixnum)
-(add-hook 'after-init-hook (lambda ()
-			     (setq gc-cons-threshold 800000)))
+(add-hook 'after-init-hook #'(lambda () (setq gc-cons-threshold 800000)))
 
-;; Package initialize occurs automatically, before `user-init-file' is
-;; loaded, but after `early-init-file'. We handle package
-;; initialization, so we must prevent Emacs from doing it early!
+;; Prevent unwanted runtime compilation for native-comp users
+(setq native-comp-deferred-compilation nil)
+
+;; Package initialize occurs automatically, before `user-init-file' is loaded
+;; but after `early-init-file'. If you want to handle package initialization,
+;; you can prevent Emacs from doing it early by uncomment next line!
 (setq package-enable-at-startup nil)
+
+;; [From DOOM Emacs]
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
 (setq load-prefer-newer noninteractive)
 
 ;; Do not resize the frame at this early stage.
@@ -21,18 +28,13 @@
 (set-language-environment 'utf-8)
 
 ;; Load theme early to avoid the blinking
-(if (>= emacs-major-version 28)
-    (load-theme 'modus-operandi t)
-  (load-theme 'leuven t))
-
-;; Cleaner GUI
-(unless (eq system-type 'darwin)
-  (menu-bar-mode -1))
-
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
-
-(when (fboundp 'scroll-bar-mode)
-  (scroll-bar-mode -1))
+;; use new modus theme when greater than 28
+;; Comment out these lines, if you use customized themes.
+;; (if (>= emacs-major-version 28)
+;;     (load-theme 'modus-vivendi t)
+;;   (load-theme 'wombat t))
 
 ;;; early-init.el ends here
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; END:
