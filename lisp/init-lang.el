bb0c7865d19d9a168c8fa049ca8b079c469ef693
mathslimin <mathslimin@gmail.com>
2022-01-19 16:29:05 +0800

    Update init-lang.el

--- lisp/init-lang.el
+++ lisp/init-lang.el
@@ -8,6 +8,7 @@
 (require 'init-go)			; golang
 (require 'init-python)			; python
 (require 'init-rust)			; rust
+(require 'init-java)			; java
 (require 'init-web)			; web development
 (use-package json-mode)			; json support
 (use-package markdown-mode)		; markdown support
@@ -17,6 +18,20 @@
 (use-package yaml-mode)			; yaml support
 
 (use-package quickrun)
+
+
+(add-hook 'java-mode-hook (lambda ()
+  (format-all-mode)
+))
+(add-hook 'go-mode-hook (lambda ()
+  (format-all-mode)
+))
+(add-hook 'c++-mode-hook (lambda ()
+  (format-all-mode)
+))
+(add-hook 'c-mode-hook (lambda ()
+  (format-all-mode)
+))
 
 (provide 'init-lang)
 
