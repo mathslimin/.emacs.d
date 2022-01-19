14f21fdedafa9969fb88e364bfc5d7fceabb02f7
mathslimin <mathslimin@gmail.com>
2022-01-19 12:08:50 +0800

    Create init-lang.el

---
+++ lisp/init-lang.el
@@ -0,0 +1,26 @@
+;;; init-lang.el --- configuration for IDE programming -*- lexical-binding: t -*-
+;;; Code:
+
+;; (require 'init-eglot)
+(require 'init-lsp)
+
+(require 'init-elisp)			; elisp
+(require 'init-go)			; golang
+(require 'init-python)			; python
+(require 'init-rust)			; rust
+(require 'init-web)			; web development
+(use-package json-mode)			; json support
+(use-package markdown-mode)		; markdown support
+(use-package protobuf-mode)		; protobuf support
+(use-package restclient			; restclient support
+  :mode (("\\.http\\'" . restclient-mode)))
+(use-package yaml-mode)			; yaml support
+
+(use-package quickrun)
+
+(provide 'init-lang)
+
+;;; init-lang.el ends here
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars unresolved)
+;; End:
